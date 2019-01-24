275e87f9a9153f4e6cc459f62340964c27570a77
Susana Bouchardet <susana.bouchardet@corp.globo.com>
2019-01-24 19:30:42 -0200

    get max X and Y

--- src/main/scala/com/timotta/spark/cv/Acquisition.scala
+++ src/main/scala/com/timotta/spark/cv/Acquisition.scala
@@ -1,20 +1,23 @@
 package com.timotta.spark.cv
 
 import breeze.linalg.DenseMatrix
-
+import breeze.optimize.LBFGSB
 class Acquisition(bounds: Array[Array[Double]], kappa: Double = 2.576, random: UniformRandom = new UniformRandom()) {
 
-  def max(gp: GaussianProcessModel, warmup: Int = 100000) {
+  def max(gp: GaussianProcessModel, warmup: Int = 100000, iters: Int = 250) {
     val xTries = randomTries(warmup)
 
     val (mean, std) = gp.predict(xTries)
-
     val ys = ucb(mean, std)
+
+    val (xMax, yMax) = xTries.zip(ys).sortBy(_._2).last
+    val xSeeds = randomTries(iters)
+
 
   }
 
-  def randomTries(warmup: Int) = {
-    0.until(warmup).map { r =>
+  def randomTries(n: Int) = {
+    0.until(n).map { r =>
       bounds.map { bound =>
         random.inRange(bound(0), bound(1))
       }.toArray
@@ -26,6 +29,10 @@ class Acquisition(bounds: Array[Array[Double]], kappa: Double = 2.576, random: U
       case (m, s) =>
         m + kappa * s
     }
+  }
+
+  def minimize(): Unit ={
+
   }
 
 }
