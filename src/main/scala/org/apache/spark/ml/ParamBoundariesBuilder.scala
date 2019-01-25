18a52195c5906e770587627a9ecaf8bdb58a2beb
Susana Bouchardet <susana.bouchardet@corp.globo.com>
2019-01-25 14:33:46 -0200

    ParamBoundariesBuilder: builder the domain boundaries

--- src/main/scala/org/apache/spark/ml/ParamBoundariesBuilder.scala
+++ src/main/scala/org/apache/spark/ml/ParamBoundariesBuilder.scala
@@ -13,19 +13,24 @@ class ParamBoundariesBuilder() extends ParamGridBuilder {
     this
   }
 
+  //TODO: validate if the lowerBoudarie < upperBoundarie
+  /**
+    * Buid a Array with two positions: First is the lower boundarie of all params; Second is the upper boundarie of all params;
+    * @return
+    */
   override def build():Array[ParamMap] = {
 
     var lowerMap = new ParamMap()
     var upperMap = new ParamMap()
     boundariesGrid.foreach{
       case (param:IntParam, v:mutable.WrappedArray[Int] ) =>
-
         lowerMap.put(param,v.head)
+        upperMap.put(param,v.last)
 
       case (param:DoubleParam, v:mutable.WrappedArray[Double] ) =>
-
+        lowerMap.put[Double](param,v.head)
+        upperMap.put[Double](param,v.last)
     }
-
 
     Array(lowerMap,upperMap)
   }
--- src/test/scala/com/timotta/spark/cv/ParamBoundariesBuilderTest.scala
+++ src/test/scala/com/timotta/spark/cv/ParamBoundariesBuilderTest.scala
@@ -7,15 +7,27 @@ class ParamBoundariesBuilderTest extends BaseTest {
 
   it should "ParamBoundariesBuilder" in  {
 
-    val pbb = new ParamBoundariesBuilder()
-
     val estimator = new LinearRegression()
 
+    val bounds = new ParamBoundariesBuilder()
+      .addGrid(estimator.regParam, Array(0.2D,0.3D))
+      .addGrid(estimator.maxIter, Array(2,10))
+      .build()
 
-    val bounds = pbb
-      .addGrid(estimator.epsilon, Array(0.2,0.3))
-      .addGrid(estimator.maxIter, Array(2,10)).build()
-    bounds.foreach(println)
+    val lowerMap = bounds.head
+    val uppermap = bounds.last
+
+    val lowerEpsilon = lowerMap.get(estimator.regParam)
+    val upperEpsilon = uppermap.get(estimator.regParam)
+
+    val lowerMaxIter = lowerMap.get(estimator.maxIter)
+    val upperMaxIter = uppermap.get(estimator.maxIter)
+
+    assert(Some(0.2)==lowerEpsilon)
+    assert(Some(0.3)==upperEpsilon)
+
+    assert(Some(2)==lowerMaxIter)
+    assert(Some(10)==upperMaxIter)
 
   }
 
